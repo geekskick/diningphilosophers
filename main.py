434cdbc4728ece860ee47dbda8f98ddd1d4c6164
geekskick <geeks_kick@yahoo.co.uk>
2020-12-23 20:56:30 +0000

    Added colour codes for lock acquisition

--- main.py
+++ main.py
@@ -36,20 +36,40 @@ class DiningFork(object):
 class TableSpace(object):
     THINKING = 'green'
     EATING = 'red'
+    ACQUIRING = 'cyan'
+    GOT = 'blue'
 
-    def __init__(self, label, detail):
+    def __init__(self, label: tk.Label, detail: tk.Label):
         self._label = label
         self._detail = detail
         self._count = 0
-        self._label.config(bg=self.THINKING)
+        self.thinking()
+
+    def acquiringFirst(self):
+        if not end.is_set():
+            self._label.config(bg=self.ACQUIRING)
+            self._updateLabel("Waiting on first fork")
+
+    def gotFirst(self):
+        if not end.is_set():
+            self._label.config(bg=self.GOT)
+            self._updateLabel("Waiting on second fork")
 
     def eating(self):
         self._count += 1
         self._label.config(bg=self.EATING)
         self._detail['text'] = str(self._count)
+        self._updateLabel("Eating")
 
     def thinking(self):
         self._label.config(bg=self.THINKING)
+        self._updateLabel("Thinking")
+
+    def _updateLabel(self, text: str):
+        text = f"({text})"
+        orig = self._label['text']
+        parts = orig.split('\n')
+        self._label['text'] = "\n".join((parts[0], text))
 
 
 def philosopher(name: str, left: DiningFork, right: DiningFork, space: TableSpace):
@@ -64,8 +84,11 @@ def philosopher(name: str, left: DiningFork, right: DiningFork, space: TableSpac
         f"{name} - Sorted[0] = {sorted_forks[0].name}, Sorted[1] = {sorted_forks[1].name}")
 
     while not end.is_set():
-        for fork in sorted_forks:
-            fork.lock.acquire()
+        space.acquiringFirst()
+        sorted_forks[0].lock.acquire()
+
+        space.gotFirst()
+        sorted_forks[1].lock.acquire()
 
         # If the GUI has ended then I don't want to try to do anything, I want to release the locks
         if not end.is_set():
@@ -93,22 +116,25 @@ forks = [DiningFork(str(i)) for i in range(len(PHILOSOPHERS))]
 
 root = tk.Tk()
 root.wm_title("Dining Philosophers")
+root.resizable(height=False, width=False)
 spaces = {}
 threads = []
 
 logger = tk.Text(root)
-logger.grid(row=3, columnspan=len(PHILOSOPHERS))
+logger.grid(row=2, columnspan=len(PHILOSOPHERS))
 logger = Logger(logger)
 
 logging.basicConfig(level=0)
 logging.getLogger(__name__).addHandler(logger)
 
+
 for c, p in enumerate(PHILOSOPHERS):
-    l = tk.Label(root, text=p,  width=10, height=5)
+    l = tk.Label(root, text=p,  width=20, height=5)
     l.grid(row=0, column=c)
 
     d = tk.Label(root, text='0')
     d.grid(row=1, column=c)
+
     spaces[p] = TableSpace(l, d)
 
 for n in range(len(forks)):
